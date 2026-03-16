cb864aabe8ae5d98cace20fc28dffa5db86f3d60
saurabhborikar13 <saurabhborikar13@gmail.com>
2026-03-17 01:56:08 +0530

    A1 done

---
+++ day1/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.4.0"
+    }
+  }
+}
+
+provider "local" {}
+
+variable "message" {
+  description = "Messsage to write into file"
+  type = string
+  default = "Hello from Terraform"
+}
+
+
+resource "local_file" "hello_file" {
+  filename = "hello.txt"
+  content  = var.message
+}
+
+output "file_path" {
+  description = "Path of the created file"
+  value       = local_file.hello_file.filename
+}
